6ac74bb1d95443b53ad9078609e8b8bed9a8b834
Gustavo Fonseca <gustavofons@gmail.com>
2013-08-02 16:32:01 -0300

    Solved import error caused by the import of scieloapi in setup.py to get the current version

--- scieloapi/__init__.py
+++ scieloapi/__init__.py
@@ -1,4 +1,10 @@
-from scieloapi import Connector, Endpoint, Client
+try:
+    from scieloapi import Connector, Endpoint, Client
+except ImportError as e:
+    if 'requests' in e.message:
+        print 'requests is not present. please run python setup.py install'
+    else:
+        raise
 
 
 __version__ = '0.3'
