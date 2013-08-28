4ea8e91add95c6f3a4c863ba57e8b25e57012bad
Gustavo Fonseca <gustavofons@gmail.com>
2013-08-28 15:44:27 -0300

    Ops, workaround to handle import errors during setup

--- scieloapi/__init__.py
+++ scieloapi/__init__.py
@@ -1,5 +1,11 @@
 __version__ = '0.4'
 __user_agent__ = 'scieloapi/%s' % __version__
 
-from .core import Connector, Endpoint, Client
+try:
+    from .core import Connector, Endpoint, Client
+except ImportError as e:
+    if 'requests' in e.message:
+        print 'requests is not present. please run python setup.py install'
+    else:
+        raise
 
